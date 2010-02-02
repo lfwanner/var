83ddb44a5c01ac51874d7a6785bd7e6ea2f1a192
Lucas <lucas@Lucas-Mini-Linux.(none)>
2010-02-01 16:31:50 -0800

    Sleep Power @ temperature data

--- tests/clock_temperature/avg.m
+++ tests/clock_temperature/avg.m
@@ -1,32 +1,28 @@
 boards=5;
-xtals=5;
+temperatures=6;
 multipliers=10;
 cd output;
 
-average_power=zeros(boards*xtals,multipliers);
-average_dmips=zeros(boards*xtals,multipliers);
+average_power=zeros(boards*multipliers,temperatures);
+temperature=zeros(boards*multipliers,temperatures);
+
+delta_over_mu=zeros(boards*multipliers,temperatures);
 
 xtal_base = 12;
-xtal_increment = 2;
 
 files=sortrows(ls);
 
-for i = 1:length(files)
+for i = 1:length(files)-1
   filename = strtrim(files(i,:));
-  if filename(1) == 'd'
-	  f_board = str2num(filename(7));
-	  f_xtal = str2num(filename(8:9));
-	  row = (f_board-1)*xtals+(f_xtal-xtal_base)/xtal_increment+1;
-	  file = dlmread(filename);
-	  mips = file(:,2)';
-      average_dmips(row,1:length(mips)) = mips;
-  end
   if filename(1) == 'b'
 	  f_board = str2num(filename(2));
-	  f_xtal = str2num(filename(4:5));
-	  f_clock = str2num(filename(7:end));
-	  row = (f_board-1)*xtals+(f_xtal-xtal_base)/xtal_increment+1;
-	  col = f_clock/f_xtal;
+	  idx_  = findstr (filename, "_");
+	  f_clock = str2num(filename(idx_(1)+1:idx_(2)-1));
+	  f_tempe = str2num(filename(idx_(2)+1:end));
+	  
+	  row=f_board*f_clock/xtal_base;
+	  col=min(find(average_power(row,:)==0));
+
 	  file = dlmread(filename);
 	  current = file(:,2)'.*1000;
 	  voltage = file(:,3)';
@@ -35,12 +31,20 @@ for i = 1:length(files)
 	  	current = -current;
 	  end 
 	  power = voltage.*current;
-	  average_power(row,col) = mean(power);	   
+	  average_power(row,col) = mean(power);	 
+	  temperature(row,col) = f_tempe;  
   end 
-end    
+end  
+
+for i = 1:boards*multipliers
+	delta_over_mu(i,:) = (average_power(i,:)-mean(average_power(i,:)))./mean(average_power(i,:));
+end  
 
 dlmwrite('average_power',average_power, '\t')
 dlmwrite('average_dmips',average_dmips, '\t')
+
+
+
 
 system('mv b* raw/')
 system('mv dm* raw/')
